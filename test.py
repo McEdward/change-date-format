9ecf5b38e79d80660a06b27d2858ed6c3e04291f
Macaulay Davies <mac114.md@gmail.com>
2019-04-19 14:48:35 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ def change_date_format(x):
     for i in x:
         if len(i) == 10:
             ans = ""
-            if "/" in i and i.find("/") == 4:
+            if "/" in i and i.find("/") == 4 and i[7] == "/":
                 ans += i[:4]
                 ans += i[5:7]
                 ans += i[8:]
                 res.append(ans)
-            elif "/" in i and i.find("/") == 2:
+            elif "/" in i and i.find("/") == 2 and i[5] == "/":
                 ans += i[6:]
                 ans += i[3:5]
                 ans += i[:2]
                 res.append(ans)
-            elif "-" in i and i.find("-") == 2:
+            elif "-" in i and i.find("-") == 2 and i[5] == "-":
                 ans += i[6:]
                 ans += i[3:5]
                 ans += i[:2]
