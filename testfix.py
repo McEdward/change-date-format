8135ff9b3159200e9671c558d24900f5f462d4ba
Macaulay Davies <mac114.md@gmail.com>
2019-04-20 15:09:38 +0100

    Add files via upload

--- testfix.py
+++ testfix.py
@@ -1,23 +1,12 @@
+from dateutil.parser import parse
 def change_date_format(x):
     res = []
+
     for i in x:
-        if len(i) == 10:
-            ans = ""
-            if "/" in i and i.find("/") == 4 and i[7] == "/":
-                ans += i[:4]
-                ans += i[5:7]
-                ans += i[8:]
-                res.append(ans)
-            elif "/" in i and i.find("/") == 2 and i[5] == "/":
-                ans += i[6:]
-                ans += i[3:5]
-                ans += i[:2]
-                res.append(ans)
-            elif "-" in i and i.find("-") == 2 and i[5] == "-":
-                ans += i[6:]
-                ans += i[:2]
-                ans += i[3:5]
-                res.append(ans)
+        if "/" in i or "-" in i:
+            p = parse(i)
+            res.append(p.strftime('%Y%m%d'))
     return res
+
 
 print(change_date_format(["2019/03/30", "15/12/2006", "11-15-2012", "20130730"]))
